82a98a2170557d41b56fe18e16ec3f2ce6a0f5c8
KoenVermeulenNL <84975944+KoenVermeulenNL@users.noreply.github.com>
2023-10-24 11:57:54 +0200

    added gstate

--- src/Controller.hs
+++ src/Controller.hs
@@ -23,8 +23,8 @@ input e gstate = return (inputKey e gstate)
 
 inputKey :: Event -> GameState -> GameState
 inputKey (EventKey (SpecialKey KeyUp) Down _ _) gstate = gstate { player = (player gstate) { positionPlayer = moveForward gstate } }
--- inputKey (EventKey (SpecialKey KeyRight) Down _ _) gstate = gstate { player = rotateShip gstate 10 }
--- inputKey (EventKey (SpecialKey KeyLeft) Down _ _) gstate = gstate { player = rotateShip gstate (-10) }
+inputKey (EventKey (SpecialKey KeyRight) Down _ _) gstate = gstate { player =  (player gstate) { directionPlayer = rotateShip gstate 10} }
+inputKey (EventKey (SpecialKey KeyLeft) Down _ _) gstate = gstate { player =  (player gstate) { directionPlayer = rotateShip gstate (-10)}  }
 inputKey (EventKey (SpecialKey KeyEsc) Down _ _) gstate = initialState
 -- inputKey (EventKey (SpecialKey KeySpace) Down _ _) gstate = fireBullet gstate
 inputKey _ gstate = gstate -- Otherwise keep the same
@@ -37,24 +37,12 @@ inputKey _ gstate = gstate -- Otherwise keep the same
 --         direction = directionBullet $ player gstate
 --         lifespan = elapsedTime gstate
 
--- -- This function returns the world with an updated angle in which the moveForward function will move
--- rotateShip :: GameState -> Int -> World
--- rotateShip gstate speed = Play (
---     Player
---         pos
---         vel
---         acc
---         (dirAngle + fromIntegral speed)
---     )
---     bullets
---     astroids
---         where
---             dirAngle = giveDir (world gstate)
---             vel = giveVelocity (world gstate)
---             acc@(accX, accY) = giveAcceleration (world gstate)
---             pos@(posX, posY) = givePIS (world gstate)
---             bullets = getBullets $ world gstate
---             astroids = getAstroids $ world gstate
+-- This function returns the world with an updated angle in which the moveForward function will move
+rotateShip :: GameState -> Float -> Direction
+rotateShip gstate speed = newDir
+        where
+            oldDir = directionPlayer (player gstate)
+            newDir = oldDir + speed
 
 
 
