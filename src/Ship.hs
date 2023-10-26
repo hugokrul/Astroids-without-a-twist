2c14bad8f69a908fa41255d7108413c78ab1a923
KoenVermeulenNL <84975944+KoenVermeulenNL@users.noreply.github.com>
2023-10-26 23:01:07 +0200

    werkt-niet

--- src/Ship.hs
+++ src/Ship.hs
@@ -1,100 +1,107 @@
 module Ship where
 
-import Model
-import Imports
-import qualified Graphics.Gloss.Data.Point.Arithmetic as PMath
-
 import qualified Data.Set as Set
+import qualified Graphics.Gloss.Data.Point.Arithmetic as PMath
+import Graphics.Gloss.Data.Vector
+import Imports
+import Model
 
 ship :: Picture
-ship = pictures
-    [
-        leftLine,
-        rightLine,
-        middleLine
+ship =
+  pictures
+    [ leftLine,
+      rightLine,
+      middleLine
     ]
+  where
     -- width : 35 height: 50
     -- bottomRight = x + 17.5, y - 25
-    where
-        leftLine    = translate (-9)    0   $ rotate 20     $ color white $ rectangleSolid 1 50
-        rightLine   = translate   9     0   $ rotate (-20)  $ color white $ rectangleSolid 1 50
-        middleLine  = translate   0  (-10)  $ rotate 90     $ color white $ rectangleSolid 1 25
+
+    leftLine = translate (-9) 0 $ rotate 20 $ color white $ rectangleSolid 1 50
+    rightLine = translate 9 0 $ rotate (-20) $ color white $ rectangleSolid 1 50
+    middleLine = translate 0 (-10) $ rotate 90 $ color white $ rectangleSolid 1 25
 
 checkDeleteShip :: Player -> Player
 checkDeleteShip player
-    | x < -380 || x > 380 || y > 220 || y < -220 = player { positionPlayer = (0, 0), lives = lives player - 1}
-    | otherwise = player
-    where
-        x = fst $ positionPlayer player
-        y = snd $ positionPlayer player
+  | x < -380 || x > 380 || y > 220 || y < -220 = player {positionPlayer = (0, 0), lives = lives player - 1}
+  | otherwise = player
+  where
+    x = fst $ positionPlayer player
+    y = snd $ positionPlayer player
 
 checkCollission :: GameState -> GameState
 checkCollission gstate = checkCollission' (player gstate) (astroids gstate) gstate
 
 checkCollission' :: Player -> [Astroid] -> GameState -> GameState
 checkCollission' p [] gstate = gstate
-checkCollission' p [a] gstate 
-    | checkCollissionShipAstroid p a = gstate { player = p { positionPlayer = (0, 0), lives = lives p - 1}}
-    | otherwise = gstate
-checkCollission' p (a:as) gstate
-    | checkCollissionShipAstroid p a = gstate { player = p { positionPlayer = (0, 0), lives = lives p - 1}}
-    | otherwise = checkCollission' p as gstate
-
+checkCollission' p [a] gstate
+  | checkCollissionShipAstroid p a = gstate {player = p {positionPlayer = (0, 0), lives = lives p - 1}}
+  | otherwise = gstate
+checkCollission' p (a : as) gstate
+  | checkCollissionShipAstroid p a = gstate {player = p {positionPlayer = (0, 0), lives = lives p - 1}}
+  | otherwise = checkCollission' p as gstate
 
 checkCollissionShipAstroid :: Player -> Astroid -> Bool
 checkCollissionShipAstroid p a =
-    pointInAstroid (x + 17.5, y - 25) a ||
-    pointInAstroid (x - 17.5, y - 25) a ||
-    pointInAstroid (x + 17.5, y - 25) a ||
-    pointInAstroid (x + 17.5, y + 25) a
-    where
-        (x, y) = positionPlayer p
+  pointInAstroid (x + 17.5, y - 25) a
+    || pointInAstroid (x - 17.5, y - 25) a
+    || pointInAstroid (x + 17.5, y - 25) a
+    || pointInAstroid (x + 17.5, y + 25) a
+  where
+    (x, y) = positionPlayer p
 
 pointInAstroid :: Point -> Astroid -> Bool
-pointInAstroid p0 a = case sizeAstroid a of 
-    Big -> pointInBox p0 p1 p2
-        where
-            p1 = (ax, ay)
-            p2 = (ax+66, ay - 66)
-            (ax, ay) = positionAstroid a
-    Medium -> pointInBox p0 p1 p2 
-        where
-            p1 = (ax, ay)
-            p2 = (ax+33, ay - 33)
-            (ax, ay) = positionAstroid a
-    Small -> pointInBox p0 p1 p2 
-        where
-            p1 = (ax, ay)
-            p2 = (ax+17, ay - 17)
-            (ax, ay) = positionAstroid a
+pointInAstroid p0 a = case sizeAstroid a of
+  Big -> pointInBox p0 p1 p2
+    where
+      p1 = (ax, ay)
+      p2 = (ax + 66, ay - 66)
+      (ax, ay) = positionAstroid a
+  Medium -> pointInBox p0 p1 p2
+    where
+      p1 = (ax, ay)
+      p2 = (ax + 33, ay - 33)
+      (ax, ay) = positionAstroid a
+  Small -> pointInBox p0 p1 p2
+    where
+      p1 = (ax, ay)
+      p2 = (ax + 17, ay - 17)
+      (ax, ay) = positionAstroid a
 
 stepPlayerState :: Player -> Float -> GameState -> Player
-stepPlayerState player time gstate = player
+stepPlayerState player time gstate = player {positionPlayer = newPos, velocityPlayer = newVel, accelarationPlayer = newAcc}
+  where
+    pos = positionPlayer player
+    vel@(vx, vy) = velocityPlayer player
+    acc@(ax, ay) = accelarationPlayer player
+    multacc = ((vx * ax), (vy * ay))
+    newPos = pos PMath.+ multacc
+    newAcc = ((ax * 0.9), (ay * 0.9))
 
 updatePosition :: GameState -> Player -> Player
 updatePosition gstate player
-    | up && right   = moveForward (player {velocityPlayer = rotateShip player 1}) (elapsedTime gstate)
-    | up && left    = moveForward (player {velocityPlayer = rotateShip player (-1)}) (elapsedTime gstate)
-    | up            = moveForward player (elapsedTime gstate)
-    | right         = player { velocityPlayer = rotateShip player 1}
-    | left          = player { velocityPlayer = rotateShip player (-1)}
-    | otherwise     = player
-    where
-        up = Set.member KeyUp (keySet gstate)
-        right = Set.member KeyRight (keySet gstate)
-        left = Set.member KeyLeft (keySet gstate)
+  | up && right = moveForward (player {velocityPlayer = rotateShip player 1}) (elapsedTime gstate)
+  | up && left = moveForward (player {velocityPlayer = rotateShip player (-1)}) (elapsedTime gstate)
+  | up = moveForward player (elapsedTime gstate)
+  | right = player {velocityPlayer = rotateShip player 1}
+  | left = player {velocityPlayer = rotateShip player (-1)}
+  | otherwise = player
+  where
+    up = Set.member KeyUp (keySet gstate)
+    right = Set.member KeyRight (keySet gstate)
+    left = Set.member KeyLeft (keySet gstate)
 
 rotateShip :: Player -> Float -> Vector
 rotateShip player speed = newVel
-        where
-            pos = positionPlayer player
-            vel = velocityPlayer player
-            newVel = rotateV (-(degToRad speed)) vel 
+  where
+    pos = positionPlayer player
+    vel = velocityPlayer player
+    newVel = rotateV (-(degToRad speed)) vel
 
 -- This function adds the direction to the current position, moving it to the front of which the ship is looking
 moveForward :: Player -> Float -> Player
-moveForward player time = player { positionPlayer = newPos, accelarationPlayer = vel}
-        where
-            pos = positionPlayer player
-            vel = velocityPlayer player
-            newPos = pos PMath.+ vel
+moveForward player time = player {positionPlayer = newPos, accelarationPlayer = vel}
+  where
+    pos = positionPlayer player
+    vel = velocityPlayer player
+    newPos = pos PMath.+ vel
