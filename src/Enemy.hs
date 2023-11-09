b44ce22a0fbdf81c67ffd9dad623f3ff07eae910
hugokrul <hugo.krul@gmail.com>
2023-11-09 13:39:38 +0100

    shoot enemy dead

--- src/Enemy.hs
+++ src/Enemy.hs
@@ -2,6 +2,7 @@ module Enemy where
 
 import Model
 import Imports
+import Collissions
 import qualified Graphics.Gloss.Data.Point.Arithmetic as PMath
 
 
@@ -19,10 +20,9 @@ stepEnemyState [] _ gstate
     where
         elapsTime = elapsedTime gstate
         timeSpan = round elapsTime `mod` 100 == 0 && round elapsTime > 1
-
-stepEnemyState (enemy:_) time gstate 
-    | odd (round (elapsedTime gstate)) = [enemy {positionEnemy = newPos, velocityEnemy = newVel, reloading = False} {-| not (enemyShot gstate)-}]
-    | otherwise = [enemy {positionEnemy = newPos, velocityEnemy = newVel}]
+stepEnemyState (enemy:_) time gstate
+    | odd (round (elapsedTime gstate)) = [enemy {positionEnemy = newPos, velocityEnemy = newVel, reloading = False} | not (enemyShot enemy gstate)]
+    | otherwise = [enemy {positionEnemy = newPos, velocityEnemy = newVel} | not (enemyShot enemy gstate)]
     where
         pos = positionEnemy enemy
         vel@(vx, vy) = velocityEnemy enemy
@@ -30,11 +30,23 @@ stepEnemyState (enemy:_) time gstate
         newVel = if magV diff <= 0.1 then (0, 0) else normalizeV diff
         diff = positionPlayer (player gstate) PMath.- pos
 
+enemyShot :: Enemy -> GameState -> Bool
+enemyShot enemy gstate = enemyShotBullets enemy buls
+    where
+        buls = bullets gstate
+
+enemyShotBullets :: Enemy -> [Bullet] -> Bool
+enemyShotBullets _ [] = False
+enemyShotBullets e (b:bs)
+    | pointInShip e (positionBullet b) && not (enemyBullet b) = True
+    | otherwise = enemyShotBullets e bs
+
+
 shootEnemyBullet :: GameState -> GameState
 shootEnemyBullet gstate
-    | not (null (enemy gstate)) = 
-        if even (round (elapsedTime gstate)) && not (reloading $ head (enemy gstate)) 
-            then gstate {bullets = newBullet : bullets gstate, enemy = [(head (enemy gstate)) {reloading = True}]} 
+    | not (null (enemy gstate)) =
+        if even (round (elapsedTime gstate)) && not (reloading $ head (enemy gstate))
+            then gstate {bullets = newBullet : bullets gstate, enemy = [(head (enemy gstate)) {reloading = True}]}
             else gstate
     | otherwise = gstate
             where
