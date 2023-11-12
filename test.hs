517166a3c5e9f82be1cba46de43bb8b4dd57b242
hugokrul <hugo.krul@gmail.com>
2023-11-12 20:53:49 +0100

    final changes

--- test.hs
+++
@@ -1,9 +0,0 @@
-import Data.Maybe
-
-deleteMaybes [] = []
-deleteMaybes [x] 
-    | isJust x = [fromJust x] 
-    | otherwise = []
-deleteMaybes (x:xs) 
-    | isJust x = fromJust x : deleteMaybes xs 
-    | otherwise = deleteMaybes xs
